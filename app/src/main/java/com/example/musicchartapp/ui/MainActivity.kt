78f9006727c7edf074315c7446d0a98a46fbd631
GioGue <giorgiogueli2001@gmail.com>
2023-03-15 09:45:43 +0100

    Loading screen

--- app/src/main/java/com/example/musicchartapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/musicchartapp/ui/MainActivity.kt
@@ -1,24 +1,28 @@
 package com.example.musicchartapp.ui
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.FontWeight.Companion.Black
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.musicchartapp.model.Track
 import com.example.musicchartapp.ui.theme.MusicChartAppTheme
 import com.example.musicchartapp.viewModel.TopTrackViewModel
+import com.example.musicchartapp.viewModel.TopTracksUIState
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,7 +44,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TrackApp(tracksViewModel: TopTrackViewModel = viewModel()){
-    TrackListScreen(tracksViewModel)
+    when (tracksViewModel.tracksUIState){
+        is TopTracksUIState.Success ->
+            TrackListScreen(tracksViewModel)
+        is TopTracksUIState.Loading -> LoadingScreen()
+        is TopTracksUIState.Error -> ErrorScreen()
+    }
 }
 
 
@@ -53,9 +62,50 @@ fun TrackListScreen(tracksViewModel: TopTrackViewModel) {
         if (tracks.isNotEmpty()) {
             TopTrackList(tracks,tracksViewModel)
         } else {
-            CircularProgressIndicator(modifier = Modifier.fillMaxWidth()) //
+            //Text("Loading...")
+            //CircularProgressIndicator(modifier = Modifier.fillMaxWidth())
+            LoadingScreen()
+
         }
     }
+}
+
+
+@Composable
+fun LoadingScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        BoxWithConstraints {
+            val size = if (maxWidth < maxHeight) maxWidth else maxHeight
+            val padding = (size / 4).coerceAtLeast(16.dp)
+            Column(horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Spacer(modifier = Modifier.height(150.dp))
+                CircularProgressIndicator(
+                    modifier = Modifier.fillMaxWidth(),
+                    strokeWidth = 4.dp,
+                    color = MaterialTheme.colors.primary
+                )
+                //Spacer(modifier = Modifier.height(padding))
+                Spacer(modifier = Modifier.height(110.dp))
+                Text(
+                    text = "Loading...",
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 24.sp
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun ErrorScreen() {
+    Text("Error retrieving tracks. TopChart selecter can not be used.")
 }
 
 @Composable
@@ -66,6 +116,7 @@ fun TopTrackList(tracks: List<Track>, tracksViewModel: TopTrackViewModel = viewM
         }
     }
 }
+
 
 
 
